4b64eaee412ff08552ead542cc3fb4fd5082ace0
Liuhaai <haixiang@iotex.io>
2023-02-21 14:09:52 -0800

    replace sdk with ws-sdk

--- walk2earn/w3bstream/src/types.rs
+++ walk2earn/w3bstream/src/types.rs
@@ -1,18 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
-pub struct Tx {
-    pub to: String,
-    pub data: String,
-    pub value: String,
-}
-
-#[derive(Serialize)]
-pub struct Call {
-    pub to: String,
-    pub data: String,
-}
-
 #[derive(Serialize, Deserialize)]
 pub struct Record {
     pub device_id: String,
